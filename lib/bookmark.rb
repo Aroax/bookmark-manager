a6caa0c9282953bf240545ee075bc76dc72a5e52
Arran Oakes <arran.oakes@gmail.com>
2021-06-23 16:32:22 +0100

    bookmarks wrapped in bookmark objects (11)

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,17 +2,26 @@ require "pg"
 
 class Bookmark
 
+  attr_reader :id, :name, :url
+
+  def initialize(id: ,name: ,url: )
+    @id  = id
+    @name = name
+    @url = url
+  end
 
   def self.show_bookmarks
     self.environment
-    result = @connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| [bookmark["name"], bookmark["url"]] }
+    bookmarks = @connection.exec("SELECT * FROM bookmarks;")
+    bookmarks.map { |bookmark| Bookmark.new(id: bookmark['id'], name: bookmark['name'], url: bookmark['url']) }
   end
 
   def self.add(name, url)
-    self.environment
-    @connection.exec("INSERT INTO bookmarks (url, name) VALUES ('#{url}', '#{name}');")
+    result = @connection.exec("INSERT INTO bookmarks (url, name) VALUES('#{url}', '#{name}') RETURNING id, name, url;")
+    Bookmark.new(id: result[0]['id'], name: result[0]['name'], url: result[0]['url'])
   end
+
+
 
   private
 
--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -3,33 +3,45 @@ require "bookmark"
 describe Bookmark do
   let(:name) { double :name }
   let(:url) { double :url }
-  subject(:bookmark) { described_class.new }
+  # subject(:bookmark) { described_class.new }
   # it "has a name" do
   #   expect(bookmark.name).to eq name
   # end
   # it "has a url" do
   #   expect(bookmark.url).to eq url
   # end
-  it 'displays a list of bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.twitter.com/', 'Twitter');")
-    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.makers.tech/', 'Makers');")
-    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.askjeeves.com/', 'AskJeeves');")
-    # (name) VALUES ('Makers');")
-
-    bookmarks = Bookmark.show_bookmarks
-
-    expect(bookmarks).to include(["Makers", "http://www.makers.tech/"])
-    expect(bookmarks).to include(["Twitter", "http://www.twitter.com/"])
-    expect(bookmarks).to include(["AskJeeves", "http://www.askjeeves.com/"])
-
-  end
-
   it 'can add a bookmark' do
     Bookmark.add(name, url)
     bookmarks = Bookmark.show_bookmarks
-    expect(bookmarks).to include(["#{name}", "#{url}"])
+    expect(bookmarks.first).to be_a Bookmark
+    expect(bookmarks.first.name).to eq "#{name}"
+    expect(bookmarks.first.url).to eq "#{url}"
+  end
+
+  it 'displays a list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.twitter.com/', 'Twitter');")
+    # connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.makers.tech/', 'Makers');")
+    # connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.askjeeves.com/', 'AskJeeves');")
+
+    bookmark = Bookmark.add("Twitter", "http://www.twitter.com/")
+     Bookmark.add("Makers", "http://www.makers.tech/")
+     Bookmark.add("AskJeeves", "http://www.askjeeves.com")
+    # (name) VALUES ('Makers');")
+
+    bookmarks = Bookmark.show_bookmarks
+    #
+    # expect(bookmarks).to include(["Makers", "http://www.makers.tech/"])
+    # expect(bookmarks).to include(["Twitter", "http://www.twitter.com/"])
+    # expect(bookmarks).to include(["AskJeeves", "http://www.askjeeves.com/"])
+
+    expect(bookmarks.length).to eq 3
+    expect(bookmarks.first).to be_a Bookmark
+    expect(bookmarks.first.id).to eq bookmark.id
+    expect(bookmarks.first.name).to eq 'Twitter'
+    expect(bookmarks.first.url).to eq 'http://www.twitter.com/'
+
   end
 
 end
